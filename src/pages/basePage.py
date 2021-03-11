cb27da66d21d48ae3b5df205a5f0af1a75fd8772
Nikita <nikaxen96@gmail.com>
2021-03-10 21:05:26 -0500

    basePage + requirements

--- src/pages/basePage.py
+++ src/pages/basePage.py
@@ -1,6 +1,9 @@
+import sys
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.action_chains import ActionChains
 
 class BasePage():
     """ Parent class for all page classes (contains generic methods) """
@@ -11,58 +14,109 @@ class BasePage():
 
     # Generic methods
     def el_get(self, locator, wait=20):
-        return WebDriverWait(self.driver, wait).until(EC.visibility_of_element_located(locator))
+        try:
+            return WebDriverWait(self.driver, wait).until(EC.visibility_of_element_located(locator))
+        except:
+            e = sys.exc_info()[0]
+            print("!!! Exception: ", e)
 
     def el_click(self, locator):
-        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator)).click()
+        try:
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator)).click()
+        except:
+            e = sys.exc_info()[0]
+            print("!!! Exception: ", e)
 
     def el_send_text(self, locator, text):
-        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
-        element.clear()
-        element.send_keys(text)
+        try:
+            element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
+            element.clear()
+            element.send_keys(text)
+        except:
+            e = sys.exc_info()[0]
+            print("!!! Exception: ", e)
 
     def el_get_text(self, locator):
-        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
-        return element.text
+        try:
+            element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
+            return element.text
+        except:
+            e = sys.exc_info()[0]
+            print("!!! Exception: ", e)
 
     def el_verify_title(self, expectedTitle):
-        WebDriverWait(self.driver, 10).until(EC.title_is(expectedTitle))
-
-    def el_is_visible(self, by_locator):
         try:
-            element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
+            WebDriverWait(self.driver, 10).until(EC.title_is(expectedTitle))
+        except:
+            e = sys.exc_info()[0]
+            print("!!! Exception: ", e)
+
+    def el_is_visible(self, locator):
+        try:
+            element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
             return True
         except:
             return False
 
-    def el_get_title(self, title):
-        WebDriverWait(self.driver, 10).until(EC.title_is(title))
-        return self.driver.title
+    def el_is_clickable(self, locator):
+        try:
+            return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(locator))
+        except:
+            e = sys.exc_info()[0]
+            print("!!! Exception: ", e)
 
-    def select_el_option(self, by_locator, menuItem):
-        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
-        for option in element.find_elements_by_tag_name('option'):
-            if option.text == menuItem:
-                option.click()
+    def el_hover(self, locator):
+        try:
+            element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
+            ActionChains(self.driver).move_to_element(element).perform()
+        except:
+            e = sys.exc_info()[0]
+            print("!!! Exception: ", e)
+
+    def el_get_title(self, title):
+        try:
+            WebDriverWait(self.driver, 10).until(EC.title_is(title))
+            return self.driver.title
+        except:
+            e = sys.exc_info()[0]
+            print("!!! Exception: ", e)
+
+    def select_el_option(self, locator, menuItem):
+        try:
+            element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
+            for option in element.find_elements_by_tag_name('option'):
+                if option.text == menuItem:
+                    option.click()
+        except:
+            e = sys.exc_info()[0]
+            print("!!! Exception: ", e)
 
     def selectItemFromList(self, DDMenuBtn, DDUL, menuItem):
-        #find DropDown button and click
-        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located(DDMenuBtn)).click()
-        #find ul of menu items
-        dd_ul = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(DDUL))
-        #put all li elements into list
-        menuItems = dd_ul.find_elements_by_tag_name("li")
-        #loop through li elements and find desired menuItem
-        for item in menuItems:
-            text = item.text.lower()
-            if text == menuItem.lower():
-                item.click()
-                print("item selected: " + text)
-                break
+        try:
+            # Find DropDown button and click
+            WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located(DDMenuBtn)).click()
+            # Find ul of menu items
+            dd_ul = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(DDUL))
+            # Put all li elements into list
+            menuItems = dd_ul.find_elements_by_tag_name("li")
+            # Loop through li elements and find desired menuItem
+            for item in menuItems:
+                text = item.text.lower()
+                if text == menuItem.lower():
+                    item.click()
+                    print("item selected: " + text)
+                    break
+        except:
+            e = sys.exc_info()[0]
+            print("!!! Exception: ", e)
 
-    def verifyElementTextIs(self, loc, expectedText):
-        el = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(loc))
-        if el.text == expectedText:
-            return True
-        else:
-            return False
+    def verifyElementTextIs(self, locator, expectedText):
+        try:
+            el = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
+            if el.text == expectedText:
+                return True
+            else:
+                return False
+        except:
+            e = sys.exc_info()[0]
+            print("!!! Exception: ", e)
